45c2e6807b68c746d8cdd125be7dcd1fe7a7b3d8
CHATGPT\drago <dragos.boboluta@yahoo.com>
2024-06-07 20:13:13 -1200

    add initial files

---
+++ HrApp/README.md
@@ -0,0 +1,2 @@
+# HrManagement-Backend
+# HR
---
+++ HrAppDataAcces/Entities/IdentityCard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrAppDataAcces.Entities
+{
+    internal class IdentityCard : BaseFile
+    {
+        public string CNP { get; set; }
+        public string Series { get; set; }
+        public string Number {  get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmitedZone { get; set; }
+
+    }
+}
